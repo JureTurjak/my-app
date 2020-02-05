ce3d0b496bfb4e737e5cce60991119d2f80f6b17
Jure <thurjak.jurex@gmail.com>
2020-02-05 19:00:17 +0100

    initial commit
    Kopiral vse datoteke v repository :)

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Dobrodošli na moji prvi REACT - JS spletni strani ;)</h1>
+    </div>
+  );
+}
+
+export default App;
